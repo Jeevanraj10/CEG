5edf35a2136b955a6605d7eabb17f00f63cc20bd
jeevan raj <jeevan.sr@lnttechservices.com>
2019-11-15 15:59:32 +0530

    My First Commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(private formBuilder: FormBuilder, private router: Router,private userService:UserService) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+  }
+  get f() {
+    return this.loginForm.controls;
+  }
+  onRegister(){
+    this.userService.onRegister();
+  }
+}
