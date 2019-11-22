718250f1d109f8974226354f5f135dc4cc8f42f7
jeevan raj <jeevan.sr@lnttechservices.com>
2019-11-22 19:51:25 +0530

    login and sign up

--- src/app/_services/auth.services.ts
+++ src/app/_services/auth.services.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 
 export interface AuthResponseData {
@@ -17,6 +18,7 @@ export interface AuthResponseData {
     providedIn: 'root'
 })
 export class AuthService {
+    user = new Subject<User>();
     constructor(private http: HttpClient) { }
 
     signUp(email: string, password: string) {
@@ -25,16 +27,13 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true
-            }).pipe(catchError(errorRes => {
-                let errorMessage = "An unknown error occured";
-                if (!errorRes.error || !errorRes.error.error) {
-                    return throwError(errorMessage);
-                }
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errorMessage = 'Email already exist';
-                }
-                return throwError(errorMessage);
+            }).pipe(catchError(this.handleError), tap(resData => {
+                this.handleAuthentication(
+                    resData.email,
+                    resData.localId,
+                    resData.idToken,
+                    +resData.expiresIn
+                );
             }));
     }
     signIn(email: string, password: string) {
@@ -42,6 +41,46 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        });
+        }).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(
+                resData.email,
+                resData.localId,
+                resData.idToken,
+                +resData.expiresIn
+            );
+        }));
     }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = "An unknown error occured";
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'Email already exist';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'Email Does not exists';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Please enter correct password';
+                break;
+
+        }
+        return throwError(errorMessage);
+    }
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(
+            new Date().getTime() + expiresIn * 1000
+        );
+        const user = new User(
+            email,
+            userId,
+            token,
+            expirationDate
+        );
+        this.user.next(user);
+    }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../_services/auth.services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy{
+  private userSub: Subscription;
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+   this.userSub = this.authService.user.subscribe(user =>{
+     this.isAuthenticated = !!user;
+     console.log(!user);
+     console.log(!!user);
+     
+     
+   });
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
